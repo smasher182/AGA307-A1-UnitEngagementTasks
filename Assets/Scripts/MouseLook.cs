90396829fba392aa6840e6ec6dcf3197693e4e4d
smasher182 <81832820+smasher182@users.noreply.github.com>
2022-03-07 15:06:36 +1100

    Week 01- Set up and player movement

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -2,5 +2,34 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    // controls mouse speed.
+    public float mouseSensitivity = 100f;
+    // reference to player to match camera movement.
+    public Transform playerBody;
+    // rotates camera on X axis with mouse Y movement.
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        // hide and lock cursor to make it invisible while clicking around in game mode.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // records mouse side to side movement 
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // decreases cam X rotation every frame with mouse Y movement.
+        xRotation -= mouseY;
+        // limits player rotation so they don't look behind.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // keeps track of xRotation so it can be clamped.
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // player rotates around Y axis along with mouseX.
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
 
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
+
+    // reference to character controller on player.
+    public CharacterController controller;
+    // reference to player speed.
+    public float speed = 12f;
+    // reference to gravity on player.
+    public float gravity = -9.81f;
+    // reference to player max jump height. 
+    public float jumpHeight = 3f;
+
+    // reference to groundCheck if the player hits the ground.
+    public Transform groundCheck;
+    // reference to radius of groundCheck sphere.
+    public float groundDistance = 0.4f;
+    // reference to masked objects that groundcheck sphere checks for.
+    public LayerMask groundMask;
+
+    // reference to current velocity of player.
+    Vector3 velocity;
+    // bool check if the player is grounded or not.
+    bool isGrounded;
+
+    private void Update()
+    {
+        // checks if the player is grounded based on a physics check.
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            // zero velocity means the player stops moving, but physics check could register before the players hits the ground.
+            // small -ve value makes sure the player is grounded.
+            velocity.y = -2f;
+        }
+
+        // creates inputs for player movement.
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // moves the player in the direction they are facing.
+        Vector3 move = transform.right * x + transform.forward * z;
+        //refers to character controller for player movement.
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //makes the player jump off the ground based on gravity and max height that it can jump.
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // adds fall velocity to the player based on gravity. 
+        velocity.y += gravity * Time.deltaTime;
+        // assigns the velocity to the controller for movement.
+        controller.Move(velocity * Time.deltaTime);
+    }
+
 }
