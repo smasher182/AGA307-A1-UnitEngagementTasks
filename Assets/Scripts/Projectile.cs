ea2bd5dd0f2aaf031204be02a6f13b49cb08f1ec
smasher182 <81832820+smasher182@users.noreply.github.com>
2022-03-31 22:02:51 +1100

    Week 02 - Added targets, projectile destroys target

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // checks if the collided gameobject has the tag "Target".
+        if (collision.gameObject.CompareTag("Target"))
+        {
+
+
+            // destroys the collided gameobject in 1 sec.
+            Destroy(collision.gameObject, 1);
+            // destroys the collider gameobject itself.
+            Destroy(this.gameObject);
+
+
+        }
+    }
+
+
+}
