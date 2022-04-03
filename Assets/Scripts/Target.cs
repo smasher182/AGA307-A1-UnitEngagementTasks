18689f715f71ee1b33e6f9006b061e1e178f81ad
smasher182 <81832820+smasher182@users.noreply.github.com>
2022-04-04 09:08:41 +1000

    Week 04 - Challenge - Move Random

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -20,7 +20,10 @@ public class Target : MonoBehaviour
 
     // reference to max distance the target can move.
     float moveDistance = 2000;
-   
+    // reference to current speed of target.
+    public float mySpeed = 4f;
+
+
 
 
 
@@ -32,11 +35,14 @@ public class Target : MonoBehaviour
         // finds the TargetManger script.
         _TM = FindObjectOfType<TargetManager>();
 
-        StartCoroutine(Move());
+        // removed after adding random move coroutine.
+        //StartCoroutine(Move());
+
+        StartCoroutine(MoveRandom(_TM.spawnPoints[Random.Range(0, _TM.spawnPoints.Length)]));
 
     }
 
-    void SetUp()
+void SetUp()
     {
         switch (targetSize)
         {
@@ -64,21 +70,40 @@ public class Target : MonoBehaviour
 
     }
 
-    IEnumerator Move()
-    {
-        // loops from 0 until the movedistance.
-        for (int i = 0; i < moveDistance; i++)
-        {
-            // moves the object in forward direcrtion.
-            transform.Translate(Vector3.forward * Time.deltaTime);
-            yield return null;
+    /* IEnumerator Move()
+     {
+         // loops from 0 until the movedistance.
+         for (int i = 0; i < moveDistance; i++)
+         {
+             // moves the object in forward direcrtion.
+             transform.Translate(Vector3.forward * Time.deltaTime);
+             yield return null;
 
+         }
+         // moves the object in the opposite direction.
+         transform.Rotate(Vector3.up * 180);
+         // waits for 1 sec after the coroutine is run.
+         yield return new WaitForSeconds(1);
+         StartCoroutine(Move());
+     }
+    */
+
+    IEnumerator MoveRandom(Transform _newPos)
+    {
+        // while the distance between the current position and the destination position is greater than 0.1f then execute...
+        while (Vector3.Distance(transform.position, _newPos.position) > 0.1f)
+        {
+            // gives the new transform position.
+            transform.position = Vector3.MoveTowards(transform.position, _newPos.position, Time.deltaTime * mySpeed);
+            //  rotates towards the new position.
+            transform.rotation = Quaternion.LookRotation(_newPos.position);
+            yield return null;
         }
-        // moves the object in the opposite direction.
-        transform.Rotate(Vector3.up * 180);
-        // waits for 1 sec after the coroutine is run.
-        yield return new WaitForSeconds(1);
-        StartCoroutine(Move());
+        // waits for 3 seconds after the coroutine has run.
+        yield return new WaitForSeconds(3);
+        // Debug.Log("Waited for 3 secs");
+        // moves the target randomly between 0 and spawnPoints length.
+        StartCoroutine(MoveRandom(_TM.spawnPoints[Random.Range(0, _TM.spawnPoints.Length)]));
     }
 
     public void DestroyTarget()
